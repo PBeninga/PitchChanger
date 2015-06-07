5aa5990146212798736f17bb81e20d67dd609b31
Patrick Beninga <patrickbeninga@gmail.com>
2015-06-06 19:00:23 -0700

    Initial Commit

---
+++ PitchChangerTests/PitchChangerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PitchChangerTests.swift
+//  PitchChangerTests
+//
+//  Created by Patrick Beninga on 6/6/15.
+//  Copyright (c) 2015 Patrick Beninga. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PitchChangerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
